bedddf2997b05a55b98a175a54aa7d5bf13f0b79
thibault maridor <t.maridor.15@groupe-esigelec.fr>
2016-03-07 19:37:24 +0100

    visualisation du film le plus populaire
    Code permettant la visualisation du film le plus populaire
    java hadoop testé sous cloudera et le fichier u.data contenant 4
    colonnes
    userid itemid rating id timestamp
    ici le prog récupere tout les itemid et les additionne par itemid
    ensuite dans le reduceur on effectue un test pour récupérer celui
    contenant le plus d'itération maxItem est ensuite renvoyé avec ca clé
    correspondant un nombre d'itération max trouvé

---
+++ essai.java
@@ -0,0 +1,79 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class essai {
+
+  public static class TokenizerMapper
+       extends Mapper<Object, Text, Text, IntWritable>{
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+    private Text userID = new Text();
+    private Text itemID = new Text();
+    private Text rating= new Text();
+    private Text timestamp= new Text();
+
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      StringTokenizer itr = new StringTokenizer(value.toString());
+      while (itr.hasMoreTokens()) {
+    	  userID.set(itr.nextToken());
+    	  itemID.set(itr.nextToken());
+    	  rating.set(itr.nextToken());
+    	  timestamp.set(itr.nextToken());
+        
+        context.write(itemID, one);
+      }
+    }
+  }
+
+  public static class IntSumReducer
+       extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
+int max=0;
+Text maxItem=new Text();
+    public void reduce(Text key, Iterable<IntWritable> values,
+                       Context context
+                       ) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+      if(sum>max)
+      {
+    	  max=sum;
+      maxItem.set(key);
+      }
+      
+  
+    }
+    @Override
+    protected void cleanup(Context context) throws IOException, InterruptedException {
+        context.write(maxItem, new IntWritable(max));
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "word count");
+    job.setJarByClass(essai.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setCombinerClass(IntSumReducer.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
